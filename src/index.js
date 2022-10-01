5914cf2ec49e724676272feb3dc76bb28c8b79ca
KW <k.wojtasinski@wp.pl>
2022-10-01 14:46:56 +0200

    Loaders styles, css, scss & Sass Compiling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import generateJoke from "./generate.joke";
+import './styles/main.scss';
 
 console.log(generateJoke());
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,23 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     entry: {
-        bunle: path.resolve(__dirname, 'src/index.js'),
+        bundle: path.resolve(__dirname, 'src/index.js'), // index js will be resolved
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js' // name of entry
+        filename: '[name].js' // name of entry as an output generated js
+    },
+    module: {
+        rules: [
+            {
+                // any file with extension scss
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
     }
 }
